2bac53f23e7cbf12b0793b220b148aa1f903b7dd
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-11-08 10:20:28 +0530

    Templates added to resume builder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
 import ResumeBuilder from "./pages/ResumeBuilder";
-import Preview from "./pages/Preview";
 import Login from "./pages/Login";
+import ResumePreview from "./pages/ResumePreview";
 
 const App = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
           <Route path="builder/:resumeId" element={<ResumeBuilder />} />
         </Route>
 
-        <Route path="view/:resumeId" element={<Preview />} />
+        <Route path="view/:resumeId" element={<ResumePreview />} />
         <Route path="login" element={<Login />} />
       </Routes>
     </>
