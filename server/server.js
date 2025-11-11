154b1e57804e1a3b57a870ef8f33f6d93e6e02b1
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-11-11 07:18:59 +0530

    1) Backend connected with DB 2) User model created 3) User controller created with register user handler

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectToDB from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+
+await connectToDB();
 
 app.get("/", (req, res) => res.send("Server is live.."));
 
