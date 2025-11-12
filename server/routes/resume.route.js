443d04bbe67a27605f072c912a17c0923b15ca9b
BinitaDuttaDec03 <binitadutta777@gmail.com>
2025-11-12 07:18:56 +0530

    Resume routes added

---
+++ server/routes/resume.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import protectRoute from "../middlewares/auth.middleware.js";
+import {
+  createResume,
+  deleteResume,
+  getPublicResumeById,
+  getResumeById,
+  updateResume,
+} from "../controllers/resume.controller.js";
+
+const resumeRouter = express.Router();
+
+resumeRouter.post("/create", protectRoute, createResume);
+resumeRouter.put("/update", upload.single("image"), protectRoute, updateResume);
+resumeRouter.delete("/delete/:resumeId", protectRoute, deleteResume);
+resumeRouter.get("/get/:resumeId", protectRoute, getResumeById);
+resumeRouter.get("/public/:resumeId", getPublicResumeById);
+
+export default resumeRouter;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectToDB from "./config/db.js";
 import userRouter from "./routes/user.route.js";
+import resumeRouter from "./routes/resume.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +14,9 @@ app.use(cors());
 await connectToDB();
 
 app.get("/", (req, res) => res.send("Server is live.."));
+
 app.use("/api/users", userRouter);
+app.use("/api/resumes", resumeRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
